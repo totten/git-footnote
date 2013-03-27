41695063d5be11a1784d05089bc9795e05f6d45c
Tim Otten <totten@civicrm.org>
2013-03-27 01:12:22 -0400

    JiraFilter / JiraFilterTest - Add support for JIRA REST API

--- src/CRM/GitFootnote/JiraFilter.php
+++ src/CRM/GitFootnote/JiraFilter.php
@@ -5,28 +5,53 @@ class JiraFilter extends AbstractWordFilter {
 
   protected $wordPattern;
   protected $url;
-  protected $toTitle;
+  protected $jiraApi;
+  protected $jiraCache;
 
   /**
    * @param string $wordPattern
    * @param string $url
-   * @param callable|NULL $toTitle
+   * @param Jira_Api|NULL $jiraApi
    */
-  public function __construct($wordPattern, $url, $toTitle = NULL) {
+  public function __construct($wordPattern, $url, $jiraApi = NULL) {
     $this->wordPattern = $wordPattern;
     $this->url = $url;
-    $this->toTitle = $toTitle;
+    $this->jiraApi = $jiraApi;
+    $this->jiraCache = array();
   }
 
   public function filterWord(CommitMessage $message, $word) {
     if (preg_match($this->wordPattern, $word)) {
-      if ($this->toTitle) {
-        $title = call_user_func($this->toTitle, $word);
+      $issue = $this->getIssue($word);
+      if ($issue) {
+        $title = $issue->getSummary();
       } else {
         $title = NULL;
       }
       $message->addLinkNote($this->url . '/' . $word, $title);
     }
     return $word;
+  }
+
+  /**
+   * @return Jira_Issue|NULL|FALSE (NULL if no service available; FALSE if invalid key)
+   */
+  protected function getIssue($key) {
+    if (! $this->jiraApi) {
+      return NULL;
+    }
+    if (! isset($this->jiraCache[$key])) {
+      $this->jiraCache[$key] = FALSE;
+      if (!preg_match('/^[A-Za-z0-9\-]+$/', $key)) {
+        throw new \Exception("Invalid JIRA key: $key");
+      }
+
+      $walker = new \Jira_Issues_Walker($this->jiraApi);
+      $walker->push("key = $key", "*navigable");
+      foreach ($walker as $k => $issue) {
+        $this->jiraCache[$key] = $issue;
+      }
+    }
+    return $this->jiraCache[$key];
   }
 }
